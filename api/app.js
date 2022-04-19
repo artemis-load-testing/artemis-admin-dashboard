6fe3673bf0383483b774a0221ad0c56956e15302
Sophie McGarity <sophiemcgarity7@gmail.com>
2022-04-19 15:40:00 -0700

    dashboard built and folder moved to api folder
    Co-authored-by: miajim <mjimenez@vt.edu>
    Co-authored-by: sophiemcgarity <sophiemcgarity7@gmail.com>

--- api/app.js
+++ api/app.js
@@ -24,13 +24,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("build"));
 
 app.use("/artemisApi", artemisApiRouter);
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function (err, req, res, next) {
